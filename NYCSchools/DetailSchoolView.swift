07339345a754f70c20cff146a52f2154b3023abe
Saiteja Alle <saitejaalle@Saitejas-MacBook-Pro-2.local>
2022-08-06 09:47:06 -0700

    Added View, unit tests and code refactoring for the optimization

---
+++ NYCSchools/DetailSchoolView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailSchoolView.swift
+//  NYCSchools
+//
+//  Created by Saiteja Alle on 8/5/22.
+//
+
+import SwiftUI
+
+struct DetailSchoolView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailSchoolView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailSchoolView()
+    }
+}
